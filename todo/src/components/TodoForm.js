42fc0d3b44bac1f16c461a65ec215a0772d2b0ac
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-18 16:42:32 +0100

    Styled the form

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,8 +1,46 @@
 import React, { createRef } from "react";
 import { func } from "prop-types";
 import { connect } from "react-redux";
+import styled from "styled-components";
 
 import { createTodo } from "../state/actionDispatchers";
+
+const StyledForm = styled.form`
+  background: #fff4f4;
+  text-align: center;
+  padding: 20px;
+  max-width: 700px;
+  margin: auto;
+
+  h3 {
+    color: #f072a9;
+    margin: 0 0 15px;
+  }
+
+  input {
+    width: 300px;
+    height: 17px;
+    background: #ffeff6;
+    border: 2px solid #ffdae7;
+    color: #f072a9;
+    padding: 5px;
+    margin: 0 5px;
+  }
+
+  button {
+    width: 100px;
+    height: 30px;
+    background: #f072a9;
+    color: #ffeff6;
+    border: 2px solid #eb3b88;
+    font-size: 12px;
+    font-weight: bold;
+  }
+
+  button:hover {
+    cursor: pointer;
+  }
+`;
 
 function TodoForm({ createTodo }) {
   const todoInput = createRef();
@@ -14,10 +52,11 @@ function TodoForm({ createTodo }) {
   };
 
   return (
-    <form onSubmit={submitTodo}>
-      <input type="text" placeholder="...todo" ref={todoInput} />
+    <StyledForm onSubmit={submitTodo}>
+      <h3>Add a task.</h3>
+      <input type="text" placeholder="...create todo" ref={todoInput} />
       <button type="submit">Add Todo</button>
-    </form>
+    </StyledForm>
   );
 }
 
