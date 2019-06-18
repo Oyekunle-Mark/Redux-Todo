90444821ff56de9c8ff62e43f3dedbc690972de1
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-18 13:24:52 +0100

    Created the redux store

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,5 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import reducer from "./state/todosReducer";
+
+const todoReducer = combineReducers({
+  todos: reducer
+});
+
+const store = createStore(
+  todoReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
